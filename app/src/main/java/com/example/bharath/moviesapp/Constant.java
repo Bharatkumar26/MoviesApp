5e4371a753bf16f2f6376aebacb176b6d9589cec
bharat <bharathkcenat26@gmail.com>
2016-04-21 08:22:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bharath/moviesapp/Constant.java
@@ -0,0 +1,9 @@
+package com.example.bharath.moviesapp;
+
+/**
+ * Created by Bharath on 4/20/2016.
+ */
+public class Constant {
+
+    public static final String IMAGE_TMDB_URL ="http://image.tmdb.org/t/p/w185/";
+}
---
+++ app/src/main/java/com/example/bharath/moviesapp/MoviesFlavor.java
@@ -0,0 +1,12 @@
+package com.example.bharath.moviesapp;
+
+public class MoviesFlavor {
+    String image;
+
+    public MoviesFlavor(String image)
+    {
+
+        this.image = image;
+    }
+
+}
